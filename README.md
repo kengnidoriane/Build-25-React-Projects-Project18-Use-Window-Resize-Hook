e4ac62f2fb63064b9e135d5a40ef610a3d8c9989
unknown <dorianekengni@gmail.com>
2024-10-11 17:07:20 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Projet 18: Window Resize Hook
+
+Ce projet React calcule et affiche la hauteur et la largeur de la fenêtre du navigateur en temps réel.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import UseWindowResizeTest from "./components/use-window-resize/test"
+
+function App() {
+
+  return (
+    <>
+      <UseWindowResizeTest />
+    </>
+  )
+}
+
+export default App
